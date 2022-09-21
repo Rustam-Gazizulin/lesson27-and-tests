c2eee25048c5d6229d6dea382313590975d91084
rgazizulin174 <rgazizulin174@gmail.com>
2022-09-21 11:40:40 +0500

    add views courses

--- my_project_part_1/courses/urls.py
+++ my_project_part_1/courses/urls.py
@@ -1,4 +1,9 @@
+from django.urls import path
+from courses.views import courses
+
+
+
 # TODO настройте здесь urls для заданий сourses, new_courses, find_by_name, who's_author
 urlpatterns = [
-
+    path("courses/", courses),
 ]
