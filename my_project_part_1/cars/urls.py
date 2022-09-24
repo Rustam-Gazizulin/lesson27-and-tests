334600691d24ff5128a67020eb7b641f0f5ab9db
rgazizulin174 <rgazizulin174@gmail.com>
2022-09-24 09:23:29 +0500

    add views search_courses 1 fix

--- my_project_part_1/cars/urls.py
+++ my_project_part_1/cars/urls.py
@@ -1,2 +1,9 @@
+from django.urls import path
+from cars import views
+
+
 # TODO настройте здесь urls для заданий get_car и search_car)
-urlpatterns = []
+urlpatterns = [
+    path('<int:pk>/', views.get_car, name='car'),
+    path('search/', views.search, name='search'),
+]
--- my_project_part_1/cars/views.py
+++ my_project_part_1/cars/views.py
@@ -1,8 +1,36 @@
+from cars.models import Car
+from django.http import JsonResponse
+
+
 def get_car(request, pk):
-    # TODO напишите view-функцию здесь (Readme.md, Задание get_car)
-    pass
+    car = Car.objects.get(pk=pk)
+    return JsonResponse({
+        "id": car.pk,
+        "slug": car.slug,
+        "name": car.name,
+        "brand": car.brand,
+        "address": car.address,
+        "description": car.description,
+        "status": car.status,
+        "created": car.created
+    })
 
 
 def search(request):
-    # TODO напишите view-функцию здесь (Readme.md, Задание car_search)
-    pass
+    cars_list = Car.objects.all()
+
+    search_brand = request.GET.get("brand", None)
+    if search_brand:
+        cars_list = cars_list.filter(brand=search_brand)
+
+    response = []
+    for car in cars_list:
+        response.append(
+            {
+                "id": car.pk,
+                "name": car.name,
+                "brand": car.brand,
+                "status": car.status,
+            }
+        )
+    return JsonResponse(response, safe=False)
